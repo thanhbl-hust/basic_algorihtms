60add3744f798884fd49753373c7a2b9c35f07c3
thanhbl-hust <lamthanhbui02@gmail.com>
2022-11-02 16:30:21 +0700

    Algorithms

--- algorithms/SORTING/quickSort/.py
+++ algorithms/SORTING/quickSort/.py
@@ -8,15 +8,21 @@ list = [x for x in range(10, 0, -1)]
 
 def getindex(list, low, high):
 
+    i = low
+    j = high - 1
     tmp = list[high]
-    i = low - 1
 
-    for j in range(low, high):
-        if list[j] <= tmp:
-            i = i + 1
+    while i < j:
+        while i < high and list[i] < tmp:
+            i += 1
+        while j > low and list[j] > tmp:
+            j -= 1
+        if i < j:
             list[i], list[j] = list[j], list[i]
-    list[i + 1], list[high] = list[high], list[i + 1]
-    return i + 1
+    if list[i] > tmp:
+        list[i], list[high] = list[high], list[i]
+    return i
+        
 
 
 def quickSort(list, low, high):
