e8bc26af09ccdf178c02db5f7696c89335c9a0eb
thanhbl-hust <lamthanhbui02@gmail.com>
2022-11-02 16:07:15 +0700

    Algorithms

---
+++ algorithms/SORTING/mergeSort/.py
@@ -0,0 +1,34 @@
+list = [x for x in range(10, 0, -1)]
+
+def mergeSort(list):
+
+    length = len(list)
+    if length == 1:
+        return list 
+    mid = length // 2 #lay so nguyen
+    left = list[:mid]
+    right = list[mid:]
+    mergeSort(left)
+    mergeSort(right)
+    i = 0
+    j = 0
+    k = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            list[k] = left[i]
+            i += 1
+        else:
+            list[k] = right[j]
+            j += 1
+        k += 1
+    while i < len(left):
+        list[k] = left[i]
+        i += 1
+        k += 1
+    while j < len(right):
+        list[k] = right[j]
+        j += 1
+        k += 1
+    return list
+list = mergeSort(list)
+print(list)
