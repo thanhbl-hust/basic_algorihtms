05799699d94298f54f616fa095ad6b23b5faca2b
thanhbl-hust <lamthanhbui02@gmail.com>
2022-11-03 13:08:19 +0700

    Algorithms

--- algorithms/RECURSIVE/factorial.cpp
+++ algorithms/RECURSIVE/factorial.cpp
@@ -1,3 +1,24 @@
+//algo - recursive - factorial
+
 #include<bits/stdc++.h>
 
-using nam
+using namespace std;
+
+long long factorial(int t){
+	if(t < 0) return -1;
+	if(t == 0 || t == 1) return 1;
+	return t * factorial(t - 1);
+	
+	//same as fibonacci, take long time if t bigger, should use dynamic;
+	
+}
+
+int main(){
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++){
+    	cout << "Factorial of " << i << ": ";
+    	cout << factorial(i) << endl;
+	}
+    return 0;
+}
